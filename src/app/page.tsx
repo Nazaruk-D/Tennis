d29aa6aa371acfcd419d504c7af21945b033e057
Nazaruk-D <nazaruk-dima@mail.ru>
2024-08-30 00:50:51 +0300

    sample authorization page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
     return (
@@ -75,20 +76,19 @@ export default function Home() {
                     </p>
                 </a>
 
-                <a
-                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                <Link
+                    href="/authorization"
                     className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
                     rel="noopener noreferrer"
                 >
                     <h2 className="mb-3 text-2xl font-semibold">
-                        Templates{' '}
+                        Authorization{' '}
                         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                             -&gt;
                         </span>
                     </h2>
                     <p className="m-0 max-w-[30ch] text-sm opacity-50">Explore starter templates for Next.js.</p>
-                </a>
+                </Link>
 
                 <a
                     href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
